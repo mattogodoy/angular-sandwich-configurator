81069e67b1acf58adaace04ad444da429383b9e5
Victor Gil Borrego <victor.gil@lbigroup.es>
2016-11-21 15:37:32 +0100

    #7 First ingredients module implementation

--- src/app/components/ingredientsSelector/ingredientsSelector.js
+++ src/app/components/ingredientsSelector/ingredientsSelector.js
@@ -1,11 +1,62 @@
-function ingredientsSelectorController() {
-  this.text = 'My brand new component!';
+function ingredientsSelectorController($log) {
+  this.$log = $log;
+  this.title = 'Choose one of these types of bread:';
+  this.$log.info('Ingredients Selector');
+  this.breads = [
+    {
+      "id": 1,
+      "name": "White Bread",
+      "price": 1,
+      "image": "https://placehold.it/150x150"
+    },
+    {
+      "id": 2,
+      "name": "Wheat Bread",
+      "price": 1,
+      "image": "https://placehold.it/150x150"
+    },
+    {
+      "id": 3,
+      "name": "Whole grain Bread",
+      "price": 1,
+      "image": "https://placehold.it/150x150"
+    },
+    {
+      "id": 4,
+      "name": "Rye Bread",
+      "price": 1,
+      "image": "https://placehold.it/150x150"
+    },
+  ];
 }
+
+ingredientsSelectorController.prototype = {
+  toggleItem: function (item) {
+    this.$log.debug('multi', this.multiSelect);
+    let index = this.selectedItems.findIndex(i => i.id === item.id);
+    let isMultiSelect = this.multiSelect;
+    let isEmpty = this.selectedItems.length === 0;
+    if(isMultiSelect || (!isMultiSelect && (index !== -1 || isEmpty))) {
+      if (index === -1) {
+          this.selectedItems.push(item);
+          item.active = true;
+      }else{
+          this.selectedItems.splice(index,1);
+          item.active = false;
+      }
+    }
+    this.$log.info('Ingridients select', this.selectedItems);
+  }
+};
 
 angular
   .module('app')
   .component('ingredientsSelector', {
     templateUrl: 'app/components/ingredientsSelector/ingredientsSelector.html',
-    controller: ingredientsSelectorController
+    controller: ingredientsSelectorController,
+    bindings: {
+      ingredients: '<',
+      multiSelect: '<',
+      selectedItems: '='
+    }
   });
-
